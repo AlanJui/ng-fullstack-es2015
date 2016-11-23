db923e5be5433aac13b39c9f85900f0f09a169f8
居正中 <AlanJui.1960@gmail.com>
2016-11-23 18:19:11 +0800

    Initial Project

---
+++ client/app/account/signup/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import SignupController from './signup.controller';
+
+export default angular.module('ngFullstackEs2015App.signup', [])
+  .controller('SignupController', SignupController)
+  .name;
